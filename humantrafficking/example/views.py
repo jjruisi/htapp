4ef5ee4b4c25e91efbe890f81594d5cc693bd90e
jjruisi <jjruisi@outlook.com>
2019-04-18 12:11:39 -0400

    search functionality works

--- humantrafficking/example/views.py
+++ humantrafficking/example/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from example.search import Search
 
 class HomePageView(TemplateView):
     template_name = "index.html"
@@ -9,9 +10,21 @@ class HomePageView(TemplateView):
 @csrf_exempt
 def test_ajax(request):
     if request.method == "POST":
+
+        # get data from ajax post
         string = request.POST.get('data')
+
+        # do search stuff here
+        searchterms = []
+        searchterms.append('bad')
+
+        newSearch = Search(searchterms, 'com', 'g147288')
+        newSearch.searchloop()
+
+        # data to return to the view
         data = {
             'data' : string
         }
+
     return JsonResponse(data)
 
